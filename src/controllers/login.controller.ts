47225fa42ea3510e607ff9edeab09c511b25ca32
sydneytu <sydneytu@usc.edu>
2018-06-11 20:44:15 +0200

    Controllers

---
+++ src/controllers/login.controller.ts
@@ -0,0 +1,47 @@
+import { repository } from "@loopback/repository";
+import { post, get, requestBody, HttpErrors } from "@loopback/rest";
+import { User } from "../models/user";
+import { UserRepository } from "../repositories/users.repository";
+import { request } from "http";
+// import {Login} from '../models/login';
+import {sign, verify} from 'jsonwebtoken';
+
+export class LoginController {
+  constructor(@repository(UserRepository.name) private userRepo: UserRepository) { }
+
+  @post('/login')
+  async login(@requestBody() login: User): Promise<any> {
+    var users = await this.userRepo.find();
+
+    var email = login.email;
+    var password = login.password;
+
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];
+      if (user.email == email && user.password == password) {
+        
+        var jwt = sign(
+          {
+            user: {
+              id: user.user_id,
+              firstname: user.firstname,
+              email: user.email
+            },
+            anything: "hello",
+          },
+          'shh',
+          {
+            issuer: 'auth.ix.co.za',
+            audience: 'ix.co.za',
+          },
+        );
+
+        return {
+          token: jwt,
+        };
+      }
+    }
+
+    throw new HttpErrors.NotFound('User not found, sorry!');
+  }
+}
---
+++ src/controllers/registration.controller.ts
@@ -0,0 +1,29 @@
+// Uncomment these imports to begin using these cool features!
+import { repository } from "@loopback/repository";
+import { post, get, requestBody, HttpErrors } from "@loopback/rest";
+import { User } from "../models/user";
+import { UserRepository } from "../repositories/users.repository";
+
+
+export class RegistrationController {
+  constructor(@repository(UserRepository.name) private userRepo: UserRepository) {}
+
+
+  @post('/registration')
+  async createNewUser(@requestBody() user: User){
+
+    // Check required fields
+    if(!user.email|| !user.password) {
+      throw new HttpErrors.BadRequest('missing data');
+    }
+
+    // Check that user does not already exist
+    let userExists: boolean = !!(await this.userRepo.count({email: user.email}));
+
+    if (userExists) {
+      throw new HttpErrors.BadRequest('user already exists');
+    }
+
+    return await this.userRepo.create(user);
+  }
+}
